d2c2fc4467d37263b62e26f48a40dccec2659efb
ShiniYanada <40791830+ShiniYanada@users.noreply.github.com>
2020-06-15 14:04:36 +0900

    スマート

--- c.py
+++ c.py
@@ -1,12 +1,2 @@
 n, k = list(map(int, input().split()))
-if n >= k:
-  n = n % k
-  if n >= k // 2 + 1:
-    n = k - n
-else:
-  while(True):
-    if n > abs(n-k):
-      n = abs(n-k)
-    else:
-      break
-print(n)
+print(min(n % k, k - (n % k)))
